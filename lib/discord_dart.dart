57376f4fed09cb08403ecfa59d6166ce7d3f0b8e
Dylan Ogle <delxhq@funo.network>
2019-04-19 00:45:47 +0100

    Base lib.

---
+++ lib/discord_dart.dart
@@ -0,0 +1,13 @@
+library discord_dart;
+
+import 'dart:async';
+import 'dart:convert';
+import 'dart:collection';
+import 'dart:io';
+import 'dart:isolate';
+import 'package:logging/logging.dart';
+import 'package:w_transport/w_transport.dart' as transport;
+
+part 'src/discord_dart/Discorddart.dart';
+
+
---
+++ lib/src/discord_dart/Discorddart.dart
@@ -0,0 +1,11 @@
+part of discord_dart;
+
+class Discorddart {
+  
+  String _token;
+
+  Discorddart() {
+    //TODO
+  }
+}
+
